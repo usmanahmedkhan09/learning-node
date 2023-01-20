aecbff3b8aa3b32cfc89bfa73323a92a19292867
usmanahmedkhan09 <usmanahmedkhan@live.com>
2023-01-20 23:03:29 +0500

    routes

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+
+const adminRoutes = require('./routes/admin')
+
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(adminRoutes)
+app.use(shopRoutes)
+
+
+app.listen(3000)
---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router()
+
+
+router.get('/add-product', (req, res, next) =>
+{
+    res.send("<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Submit</button></form>")
+})
+
+router.post('/product', (req, res, next) =>
+{
+    const { title } = req.body
+    res.redirect('/')
+})
+
+module.exports = router
